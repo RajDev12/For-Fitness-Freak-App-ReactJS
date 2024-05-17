1bbf57d3b56719275c32f57886845064d22bef9b
RajDev12 <rjnath98@gmail.com>
2024-05-17 17:12:54 +0530

    adding banner section

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { Box, Stack, Typography, Button } from '@mui/material'
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
     <Box sx={{
-      mt: {lg:'212px', xs:'70px'},
-      ml:{sm:'50px'}
+      mt: { lg: '212px', xs: '70px' },
+      ml: { sm: '50px' }
     }} position="relative" p="20px">
-<Typography color="#FF2625" 
-fontWeight="600" fontSize="26px">
-  Fitness Club
-</Typography >
-<Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-  Sweet, Smile <br/> and Repeat   
-</Typography>
-<Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-  Check out the most effective exercises   
-</Typography>
+      <Typography color="#FF2625"
+        fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography >
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+        Sweet, Smile <br /> and Repeat
+      </Typography>
+      <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px" mb={3}>
+        Check out the most effective exercises
+      </Typography>
+      <Button variant="contained" color='error'>Explore Exercises</Button>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+      
 
     </Box>
   )
